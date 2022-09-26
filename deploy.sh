06e51b433b1073b2303e84b6fa54852ac30f3e51
gaoheng <gao.heng@xiaoying.com>
2022-09-26 15:00:26 +0800

    add deploy 3

--- deploy.sh
+++ deploy.sh
@@ -1,25 +1,21 @@
 #!/usr/bin/env sh
 
-# 忽略错误
+# 确保脚本抛出遇到的错误
 set -e
 
-# 构建
-npm run docs:build
+# 生成静态文件
+yarn run docs:build
 
-# 进入待发布的目录
+# 进入生成的文件夹
 cd docs/.vitepress/dist
 
-# 如果是发布到自定义域名
-# echo 'www.example.com' > CNAME
+#创建.nojekyll 防止Github Pages build错误
+touch .nojekyll
 
 git init
 git add -A
 git commit -m 'deploy'
 
-# 如果部署到 https://<USERNAME>.github.io
-# git push -f git@github.com:<USERNAME>/<USERNAME>.github.io.git master
-
-# 如果是部署到 https://<USERNAME>.github.io/<REPO>
-git push -f git@github.com:youmikuang/web-frontend-study.git master:gh-pages
+git push -f "https://github.com/youmikuang/web-frontend-study.git" main:gh-pages
 
 cd -
